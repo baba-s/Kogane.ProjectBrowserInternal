dee42477f9a26384a308b80df296eb02eba6257c
baba_s <shotababa1206@gmail.com>
2020-07-16 10:58:56 +0900

    first commit

---
+++ Editor/ProjectBrowserInternal.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+namespace Kogane
+{
+	public static class ProjectBrowserInternal
+	{
+		public static void SetSearch( string searchString )
+		{
+			var assembly           = typeof( Editor ).Assembly;
+			var projectBrowserType = assembly.GetType( "UnityEditor.ProjectBrowser" );
+			var projectBrowser     = EditorWindow.GetWindow( projectBrowserType );
+
+			var setSearchMethodInfo = projectBrowserType.GetMethod
+			(
+				name: "SetSearch",
+				bindingAttr: BindingFlags.Public | BindingFlags.Instance,
+				binder: null,
+				types: new[] { typeof( string ) },
+				modifiers: null
+			);
+
+			setSearchMethodInfo.Invoke
+			(
+				obj: projectBrowser,
+				parameters: new object[] { searchString }
+			);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# UniProjectBrowserInternal
+
+Project ビューの internal な機能を呼び出せるようにするクラス
+
+## 使用例
+
+```cs
+using Kogane;
+using UnityEditor;
+
+public class Example
+{
+    [MenuItem( "Tools/Hoge" )]
+    private static void Hoge()
+    {
+        // Project ビューでアセットを検索
+        ProjectBrowserInternal.SetSearch( "ピカチュウ" );
+    }
+}
+```
