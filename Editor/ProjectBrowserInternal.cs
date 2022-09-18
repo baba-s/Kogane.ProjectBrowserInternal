d028e993182e45c198dcbc854e14a51cc1484f5b
baba-s <shotababa1206@gmail.com>
2022-09-18 10:52:03 +0900

    update

--- Editor/ProjectBrowserInternal.cs
+++ Editor/ProjectBrowserInternal.cs
@@ -1,17 +1,36 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
+using UnityEngine;
 
 namespace Kogane
 {
     public static class ProjectBrowserInternal
     {
+        private static readonly Type PROJECT_BROWSER_TYPE = typeof( Editor ).Assembly.GetType( "UnityEditor.ProjectBrowser" );
+
+        public static string SearchFieldText
+        {
+            get
+            {
+                var projectBrowser = GetProjectBrowser();
+
+                var fieldInfo = PROJECT_BROWSER_TYPE.GetField
+                (
+                    name: "m_SearchFieldText",
+                    bindingAttr: BindingFlags.NonPublic | BindingFlags.Instance
+                );
+
+                return ( string )fieldInfo.GetValue( projectBrowser );
+            }
+        }
+
         public static void SetSearch( string searchString )
         {
-            var assembly           = typeof( Editor ).Assembly;
-            var projectBrowserType = assembly.GetType( "UnityEditor.ProjectBrowser" );
-            var projectBrowser     = EditorWindow.GetWindow( projectBrowserType );
+            var projectBrowser = GetProjectBrowser();
 
-            var setSearchMethodInfo = projectBrowserType.GetMethod
+            var methodInfo = PROJECT_BROWSER_TYPE.GetMethod
             (
                 name: "SetSearch",
                 bindingAttr: BindingFlags.Public | BindingFlags.Instance,
@@ -20,11 +39,19 @@ namespace Kogane
                 modifiers: null
             );
 
-            setSearchMethodInfo.Invoke
+            methodInfo.Invoke
             (
                 obj: projectBrowser,
                 parameters: new object[] { searchString }
             );
+        }
+
+        private static EditorWindow GetProjectBrowser()
+        {
+            return Resources
+                    .FindObjectsOfTypeAll<EditorWindow>()
+                    .FirstOrDefault( x => x.GetType().ToString() == "UnityEditor.ProjectBrowser" )
+                ;
         }
     }
 }
