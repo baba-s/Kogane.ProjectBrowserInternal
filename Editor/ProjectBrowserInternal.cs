2b116e552c59a7e88113767b249dbeb34f535d27
baba-s <shotababa1206@gmail.com>
2022-09-20 19:37:33 +0900

    update

--- Editor/ProjectBrowserInternal.cs
+++ Editor/ProjectBrowserInternal.cs
@@ -10,19 +10,30 @@ namespace Kogane
     {
         private static readonly Type PROJECT_BROWSER_TYPE = typeof( Editor ).Assembly.GetType( "UnityEditor.ProjectBrowser" );
 
+        // https://github.com/Unity-Technologies/UnityCsReference/blob/2022.1/Editor/Mono/ProjectBrowser.cs#L114
+        private static readonly FieldInfo SEARCH_FIELD_TEXT_FIELD_INFO = PROJECT_BROWSER_TYPE.GetField
+        (
+            name: "m_SearchFieldText",
+            bindingAttr: BindingFlags.NonPublic | BindingFlags.Instance
+        );
+
+        // https://github.com/Unity-Technologies/UnityCsReference/blob/2022.1/Editor/Mono/ProjectBrowser.cs#L531
+        private static readonly MethodInfo SET_SEARCH_METHOD_INFO = PROJECT_BROWSER_TYPE.GetMethod
+        (
+            name: "SetSearch",
+            bindingAttr: BindingFlags.Public | BindingFlags.Instance,
+            binder: null,
+            types: new[] { typeof( string ) },
+            modifiers: null
+        );
+
         public static string SearchFieldText
         {
             get
             {
                 var projectBrowser = GetProjectBrowser();
 
-                var fieldInfo = PROJECT_BROWSER_TYPE.GetField
-                (
-                    name: "m_SearchFieldText",
-                    bindingAttr: BindingFlags.NonPublic | BindingFlags.Instance
-                );
-
-                return ( string )fieldInfo.GetValue( projectBrowser );
+                return ( string )SEARCH_FIELD_TEXT_FIELD_INFO.GetValue( projectBrowser );
             }
         }
 
@@ -30,20 +41,7 @@ namespace Kogane
         {
             var projectBrowser = GetProjectBrowser();
 
-            var methodInfo = PROJECT_BROWSER_TYPE.GetMethod
-            (
-                name: "SetSearch",
-                bindingAttr: BindingFlags.Public | BindingFlags.Instance,
-                binder: null,
-                types: new[] { typeof( string ) },
-                modifiers: null
-            );
-
-            methodInfo.Invoke
-            (
-                obj: projectBrowser,
-                parameters: new object[] { searchString }
-            );
+            SET_SEARCH_METHOD_INFO.Invoke( projectBrowser, new object[] { searchString } );
         }
 
         private static EditorWindow GetProjectBrowser()
